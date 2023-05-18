dac9d8b5bfec4b4f50ca63a808b8b46409b3c0ff
lndslm <129811256+lndslm@users.noreply.github.com>
2023-05-18 08:34:49 -0400

    Completed the changeBackgroundColor function and displayed the forecast details using the API

--- src/index.js
+++ src/index.js
@@ -32,26 +32,56 @@ function displayDate(date) {
   return formattedDate;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
 
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col">
-              <div class="weather-forecast-date">${day}</div>
-              <div class="weather-forecast-icon">⛅</div>
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col">
+              <div class="weather-forecast-date">${formatDay(
+                forecastDay.dt
+              )}</div>
+              <img src = "https://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png"
+              alt = ""
+              width = "42"
+              />
               <div class="weather-forecast-temperature">
-                <span class="weather-forecast-temperature-max">6°</span>
-                <span class="weather-forecast-temperature-min">-3°</span>
+                <span class="weather-forecast-temperature-max">${Math.round(
+                  forecastDay.temp.max
+                )}°</span>
+                <span class="weather-forecast-temperature-min">${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
             </div>
             </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coord) {
+  let latitude = coord.lat;
+  let longitude = coord.lon;
+  let apiKey = "d1a86552de255334f6117b348c4519bd";
+  let units = "metric";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiURL).then(displayForecast);
 }
 
 function handleSubmit(event) {
@@ -73,17 +103,73 @@ function changeBackgroundColor(response) {
     response.data.weather[0].icon === "01d" ||
     response.data.weather[0].icon === "01n"
   ) {
-    weatherBackgroundColor.style.backgroundColor = "#ffffff";
+    weatherBackgroundColor[0].style.backgroundImage = `linear-gradient(to top, rgb(255, 207, 84) 11.2%, rgb(255, 158, 27) 91.1%)`;
+    weatherBackgroundColor[1].style.backgroundImage = `linear-gradient(to top, rgb(255, 207, 84) 11.2%, rgb(255, 158, 27) 91.1%)`;
+  } else {
+    if (
+      response.data.weather[0].icon === "02d" ||
+      response.data.weather[0].icon === "02n"
+    ) {
+      weatherBackgroundColor[0].style.backgroundImage = `linear-gradient(179deg,rgb(253, 220, 155) 26.2%,rgb(255, 215, 165) 48.5%)`;
+      weatherBackgroundColor[1].style.backgroundImage = `linear-gradient(179deg,rgb(253, 220, 155) 26.2%,rgb(255, 215, 165) 48.5%)`;
+    } else {
+      if (
+        response.data.weather[0].icon === "03d" ||
+        response.data.weather[0].icon === "03n"
+      ) {
+        weatherBackgroundColor[0].style.backgroundImage = `linear-gradient(to top, #c4c5c7 0%, #dcdddf 52%, #ebebeb 100%)`;
+        weatherBackgroundColor[1].style.backgroundImage = `linear-gradient(to top, #c4c5c7 0%, #dcdddf 52%, #ebebeb 100%)`;
+      } else {
+        if (
+          response.data.weather[0].icon === "04d" ||
+          response.data.weather[0].icon === "04n"
+        ) {
+          weatherBackgroundColor[0].style.backgroundImage = `linear-gradient(-180deg,rgba(0, 0, 0, 0.5) -20%,rgba(255, 255, 255, 0.5) 180%)`;
+          weatherBackgroundColor[1].style.backgroundImage = `linear-gradient(-180deg,rgba(0, 0, 0, 0.5) -20%,rgba(255, 255, 255, 0.5) 180%)`;
+        } else {
+          if (
+            response.data.weather[0].icon === "09d" ||
+            response.data.weather[0].icon === "09n"
+          ) {
+            weatherBackgroundColor[0].style.backgroundImage = `linear-gradient(180.3deg,rgb(110, 136, 161) 5.5%,rgb(221, 221, 221) 150%)`;
+            weatherBackgroundColor[1].style.backgroundImage = `linear-gradient(180.3deg,rgb(110, 136, 161) 5.5%,rgb(221, 221, 221) 150%)`;
+          } else {
+            if (
+              response.data.weather[0].icon === "10d" ||
+              response.data.weather[0].icon === "10n"
+            ) {
+              weatherBackgroundColor[0].style.backgroundImage = `linear-gradient(179.7deg,rgb(144, 175, 202) 2.9%,rgb(197, 214, 227) 97.1%)`;
+              weatherBackgroundColor[1].style.backgroundImage = `linear-gradient(179.7deg,rgb(144, 175, 202) 2.9%,rgb(197, 214, 227) 97.1%)`;
+            } else {
+              if (
+                response.data.weather[0].icon === "11d" ||
+                response.data.weather[0].icon === "11n"
+              ) {
+                weatherBackgroundColor[0].style.backgroundImage = `linear-gradient(178.6deg,rgb(20, 36, 50) -130%,rgb(124, 143, 161) 83.8%)`;
+                weatherBackgroundColor[1].style.backgroundImage = `linear-gradient(178.6deg,rgb(20, 36, 50) -130%,rgb(124, 143, 161) 83.8%)`;
+              } else {
+                if (
+                  response.data.weather[0].icon === "13d" ||
+                  response.data.weather[0].icon === "13n"
+                ) {
+                  weatherBackgroundColor[0].style.backgroundImage = `linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)`;
+                  weatherBackgroundColor[1].style.backgroundImage = `linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)`;
+                } else {
+                  if (
+                    response.data.weather[0].icon === "50d" ||
+                    response.data.weather[0].icon === "50n"
+                  ) {
+                    weatherBackgroundColor[0].style.backgroundImage = `linear-gradient(to bottom,#d5d4d0 0%,#d5d4d0 1%,#eeeeec 31%,#efeeec 75%,#e9e9e7 100%)`;
+                    weatherBackgroundColor[1].style.backgroundImage = `linear-gradient(to bottom,#d5d4d0 0%,#d5d4d0 1%,#eeeeec 31%,#efeeec 75%,#e9e9e7 100%)`;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
-}
-
-function getForecast(coord) {
-  let latitude = coord.lat;
-  let longitude = coord.lon;
-  let apiKey = "d1a86552de255334f6117b348c4519bd";
-  let units = "metric";
-  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
-  axios.get(apiURL).then(displayForecast);
 }
 
 function displayWeatherConditions(response) {
@@ -141,6 +227,8 @@ function displayCelsiusTemperature(event) {
   celsiusLink.classList.add("active");
   farenheitLink.classList.remove("active");
 }
+
+celsiusTemperature = null;
 
 let currentDate = document.querySelector(".date");
 currentDate.innerHTML = displayDate(new Date());
